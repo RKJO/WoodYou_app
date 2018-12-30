c3b4e420deaa680bdf527164bfad678b8ed8e0ec
RKJO <rkjozwiak@gmail.com>
2018-12-30 16:26:09 +0100

    added basic urls, views, tamplates

---
+++ materials/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.MaterialsListView.as_view(), name="materials"),
+]
--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.template.response import TemplateResponse
+from django.views import View
+
 
 # Create your views here.
+
+class MaterialsListView(View):
+
+    def get(self, request):
+        return TemplateResponse(request, 'materials/materials_list.html')
+
+
+class MaterialsDetailView(View):
+
+    def get(self, request):
+        return TemplateResponse(request, 'materials/material_details.html')
