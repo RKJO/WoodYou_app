8734cec95814efbc5ec2f28675fc972c6a488ac6
RKJO <rkjozwiak@gmail.com>
2019-01-02 17:20:58 +0100

    register all models in admin area

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product, Material
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id',)
+    list_filter = ('id', 'name')
+    list_editable = ('name',)
+    list_per_page = 25
+
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price_lm', 'price_m2', 'category_name', )
+    list_display_links = ('id', 'name')
+    list_filter = ('category_name',)
+    list_editable = ('price_lm', 'price_m2')
+    search_fields = ('name', 'category_name')
+    list_per_page = 25
+
+admin.site.register(Product, ProductAdmin)
+
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category', 'name', 'price')
+    list_display_links = ('id', 'name')
+    list_filter = ('category',)
+    list_editable = ('price',)
+    search_fields = ('name', 'category')
+    list_per_page = 25
+
+admin.site.register(Material, MaterialAdmin)
