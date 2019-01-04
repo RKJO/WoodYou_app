a91b7202a375f193149131c565f9d8ec1e4fafe3
RKJO <rkjozwiak@gmail.com>
2019-01-04 19:22:50 +0100

    added propper values to functions

--- calculations/models.py
+++ calculations/models.py
@@ -2,14 +2,17 @@ from django.db import models
 from materials.models import AddAndCreate
 
 class Assembly(AddAndCreate):
-    assembly_typ = models.CharField('Typ zabudowy', max_length=100)        price_m2 = models.DecimalField(verbose_name=u'Cena m² (PLN)', max_digits=5, decimal_places=2)
+    assembly_typ = models.CharField('Typ zabudowy', max_length=100)
+    price_m2 = models.DecimalField(verbose_name=u'Cena m² (PLN)', max_digits=5, decimal_places=2)
     description = models.TextField('Opis produktu', blank=True)
 
     class Meta:
-        verbose_name = "Montaż"
+        verbose_name = "Typ montażu"
+        verbose_name_plural = "Typy montażu"
+
 
     def __str__(self):
-        return self.name
+        return self.assembly_typ
 
     def __unicode__(self):
-        return self.name
+        return self.assembly_typ
