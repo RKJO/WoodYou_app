cb4df8a0e038e7ed93afbb953828a21cc5df523e
RKJO <rkjozwiak@gmail.com>
2019-01-04 14:33:17 +0100

    added item to model realizations

--- realizations/models.py
+++ realizations/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from materials.models import AddAndCreate, Product, Material
+from calculations.models import Assembly
 
 def get_upload_realization_path(self, filename):
         return os.path.join("photos/{}/{}".format(self.used_wood, self.location), filename)
@@ -15,6 +16,7 @@ class Ralization(AddAndCreate):
     used_materials1 = models.ForeignKey(Material, on_delete=models.DO_NOTHING, verbose_name='Użyte materiały (1)', null=True, blank=True)
     used_materials1 = models.ForeignKey(Material, on_delete=models.DO_NOTHING, verbose_name='Użyte materiały (2)', null=True, blank=True)
     used_materials1 = models.ForeignKey(Material, on_delete=models.DO_NOTHING, verbose_name='Użyte materiały (3)', null=True, blank=True)
+    assembly_type = models.ForeignKey(Assembly, on_delete=models.DO_NOTHING, verbose_name='Użyte materiały (3)', null=True, blank=True)
     photo_main = models.ImageField('Obraz gółwny', upload_to=get_upload_path)
     photo_2 = models.ImageField('Obraz 2', upload_to=get_upload_path, blank=True)
     photo_3 = models.ImageField('Obraz 3', upload_to=get_upload_path, blank=True)
