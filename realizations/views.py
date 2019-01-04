878538d90763d3b2bca1694afc01510903ffd6f8
RKJO <rkjozwiak@gmail.com>
2019-01-04 19:14:33 +0100

    added besic view and template

--- realizations/views.py
+++ realizations/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 from django.views import View
-
-# Create your views here.
+from .models import Ralization
+from django.core.paginator import Paginator
 
 class RealizationsListView(View):
 
     def get(self, request):
 
-        return TemplateResponse(request, 'realizations/realizations_list.html')
+        realizations = Ralization.objects.order_by('-realization_date')
+
+        paginator = Paginator(realizations, 6)
+        page = request.GET.get('page')
+        paged_realizations = paginator.get_page(page)
+
+        context = {
+            'realizations' : paged_realizations,
+        }
+
+        return TemplateResponse(request, 'realizations/realizations_list.html', context)
