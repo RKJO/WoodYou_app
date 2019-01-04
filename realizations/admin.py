53e0d1a2dc91ba9055a7073bfe0b95a28309938b
RKJO <rkjozwiak@gmail.com>
2019-01-04 19:16:34 +0100

    addded relization admin area

--- realizations/admin.py
+++ realizations/admin.py
@@ -3,7 +3,11 @@ from .models import Ralization
 
 # @admin.register(Ralization)
 class RalizationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-
+    list_display = ('id', 'name', 'cost', 'location', 'realization_date')
+    list_display_links = ('id', 'name')
+    list_filter = ('used_wood',)
+    list_editable = ('cost',)
+    search_fields = ('name', 'used_wood', 'location')
+    list_per_page = 25
 
 admin.site.register(Ralization, RalizationAdmin)
