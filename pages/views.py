31c0944e2f9d93d7e862ce4b1e8e211c94ec9536
RKJO <rkjozwiak@gmail.com>
2019-01-04 19:26:02 +0100

    added humanize and dynamic way of realizations

--- pages/views.py
+++ pages/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 from django.template.response import TemplateResponse
 from django.views import View
 
+from realizations.models import Ralization
 
-# Create your views here.
 
 class IndexView(View):
 
     def get(self, request):
-        return TemplateResponse(request, 'pages/index.html')
+
+        realizations = Ralization.objects.order_by('-realization_date')[:3]
+
+        ctx = {
+            'realizations' : realizations
+        }
+
+        return TemplateResponse(request, 'pages/index.html', ctx)
