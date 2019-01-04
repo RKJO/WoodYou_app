ad871c5d4c6ae346f2004c1a2d67eb3f0f9327f1
RKJO <rkjozwiak@gmail.com>
2019-01-04 12:09:51 +0100

    added base view and template

--- realizations/views.py
+++ realizations/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.template.response import TemplateResponse
+from django.views import View
 
 # Create your views here.
+
+class RealizationsListView(View):
+
+    def get(self, request):
+
+        return TemplateResponse(request, 'realizations/realizations_list.html')
