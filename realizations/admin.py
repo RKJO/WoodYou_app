677fd4ce1fe6c471efe7d8520f471ddbee3c44d8
RKJO <rkjozwiak@gmail.com>
2019-01-15 15:40:21 +0100

    update realization admin

--- realizations/admin.py
+++ realizations/admin.py
@@ -4,8 +4,8 @@ from .models import Ralization
 # @admin.register(Ralization)
 class RalizationAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'cost', 'location', 'realization_date')
-    list_display_links = ('id', 'name')
-    list_filter = ('used_wood',)
+    list_display_links = ('name',)
+    list_filter = ('used_wood', 'category_name')
     list_editable = ('cost',)
     search_fields = ('name', 'used_wood', 'location')
     list_per_page = 25
