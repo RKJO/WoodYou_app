a37fc8aebda1476500086bbd2320d80152997064
RKJO <rkjozwiak@gmail.com>
2019-01-17 18:00:29 +0100

    implement values to  template

--- calculations/admin.py
+++ calculations/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Assembly
 
 class AssemblyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'assembly_typ', 'price_m2')
+    list_display = ('id', 'assembly_type', 'price_m2')
     ordering = ('id',)
-    list_display_links = ('id', 'assembly_typ')
+    list_display_links = ('id', 'assembly_type')
     list_editable = ('price_m2',)
     list_per_page = 25
 
