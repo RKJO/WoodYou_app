3f56a88f4540e6a74999cae69a288ecf821ed1e0
RKJO <rkjozwiak@gmail.com>
2019-01-18 17:47:41 +0100

    implement for in view

--- calculations/views.py
+++ calculations/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 from django.views import View
-
 from decimal import *
-
 
 from materials.models import Product, Material
 from .models import Assembly
@@ -11,33 +9,29 @@ from .forms import CalculateForm
 
 class CalculationView(View):
 
-    wood_species = Product.objects.filter(category_name__product_type=1).filter(used_for_calculate=True).order_by('-name')
-    assembly_types = Assembly.objects.order_by('price_m2')
-
     def get(self, request):
 
         context = {
-            'wood_species' : self.wood_species,
-            'assembly_types' : self.assembly_types,
+            'form' : CalculateForm(),
         }
 
         return TemplateResponse(request, 'calculations/calculation_page.html', context)
 
     def post(self, request):
-        if request.method == 'POST':
-            surface = request.POST['surface']
-            wood = request.POST.get('wood', None)
-            where_to = request.POST.get('where_to', None)
+        form = CalculateForm(data=request.POST)
+        context = {'form': form}
 
-        calculations = self.calculate_cost(surface, wood, where_to)
+        if form.is_valid():
+            surface = form.cleaned_data['surface']
+            wood = form.cleaned_data['wood']
+            where_to = form.cleaned_data['where_to']
 
-        context = {
-            'wood_species' : self.wood_species,
-            'assembly_types' : self.assembly_types,
-            'values' : request.POST,
-            'calculations' : calculations,
+            calculations = self.calculate_cost(surface, wood, where_to)
 
-        }
+            context.update({
+                'calculations': calculations,
+                'values' : form.cleaned_data,
+            })
 
         return TemplateResponse(request, 'calculations/calculation_page.html', context)
 
@@ -45,10 +39,11 @@ class CalculationView(View):
         # used wood
         wood_price = self.wood_species.filter(name=wood)[0].price_m2
         wood_cost = wood_price * Decimal(surface)
+
         # used joist
         joist_price = Product.objects.filter(category_name__product_type=2).filter(used_for_calculate=True).order_by('price_lm')[0].price_lm
         joist_cost = Decimal(joist_price * Decimal('2.5') * Decimal(surface)).quantize(Decimal('.01'), rounding=ROUND_UP)
-        
+
         # assembly
         assembly_price = self.assembly_types.filter(assembly_type=where_to)[0].price_m2
         assembly_cost = assembly_price * Decimal(surface)
@@ -63,8 +58,6 @@ class CalculationView(View):
 
         # total
         total_cost = sum([wood_cost, joist_cost, screws_cost, oil_cost, assembly_cost])
-
-
 
         return {
             'wood_price' : wood_price,
