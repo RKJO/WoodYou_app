cbe1c1d50b9607c785c1300effe688eea8bd8f7a
RKJO <rkjozwiak@gmail.com>
2019-01-29 11:49:15 +0100

    added settings to messages

--- woodyou_app/settings.py
+++ woodyou_app/settings.py
@@ -138,6 +138,12 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
 # Email config
 EMAIL_HOST = 'mail19.mydevil.net'
 EMAIL_PORT = 465
