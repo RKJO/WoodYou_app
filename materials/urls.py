8877e55b1912838ce57abfa4514d726d19d8c270
RKJO <rkjozwiak@gmail.com>
2019-01-30 12:29:49 +0100

    added urls and views for material detail  page

--- materials/urls.py
+++ materials/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.MaterialsListView.as_view(), name="materialy"),
+    path('product/<str:product_name>', views.ProductDetailView.as_view(), name='produkt'),
+    path('<str:material_name>', views.MaterialDetailView.as_view(), name='material'),
+
 ]
--- materials/views.py
+++ materials/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template.response import TemplateResponse
 from django.views import View
 from .models import Product, Material, ProductCategory, MaterialCategory
@@ -11,8 +11,8 @@ class MaterialsListView(View):
 
         products_categories = ProductCategory.objects.order_by('id')
         materials_categories = MaterialCategory.objects.order_by('id')
-        materials = Material.objects.all()
-        products = Product.objects.all()
+        materials = Material.objects.order_by('price')
+        products = Product.objects.order_by('price_m2')
 
         ctx = {
             'materials' : materials,
@@ -23,8 +23,30 @@ class MaterialsListView(View):
 
         return TemplateResponse(request, 'materials/materials_list.html', ctx)
 
+class ProductDetailView(View):
 
-class MaterialsDetailView(View):
+    def get(self, request, product_name):
 
-    def get(self, request):
-        return TemplateResponse(request, 'materials/material_details.html')
+        product = get_object_or_404(Product, name=product_name)
+        photos = [product.photo_2, product.photo_3, product.photo_4, product.photo_5]
+
+        context = {
+            'product' : product,
+            'thumbnails' : [thumbnail for thumbnail in photos if thumbnail],
+        }
+
+        return TemplateResponse(request, 'materials/product_details.html', context)
+
+class MaterialDetailView(View):
+
+    def get(self, request, material_name):
+
+        material = get_object_or_404(Material, name=material_name)
+        photos = [material.photo_2, material.photo_3 ,material.photo_4, material.photo_5]
+
+        context = {
+            'material' : material,
+            'thumbnails' : [thumbnail for thumbnail in photos if thumbnail],
+        }
+
+        return TemplateResponse(request, 'materials/material_details.html', context)
